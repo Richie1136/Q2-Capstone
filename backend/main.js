f57252f20a00e4ba69fc25a71dc8d90c5ba60da3
Richie1136 <Richiemajor1@gmail.com>
2021-01-05 11:01:41 -0500

    Worked on a few API requests but will need to add more

--- backend/main.js
+++ backend/main.js
@@ -9,11 +9,33 @@ app.use(bodyParser.json())
 
 app.all("./dinos", dinoController)
 app.get("./dinos/:id", dinoController)
+app.get("/", (req, res) => {
+  res.send("Hello from my cool server");
+});
+app.get("/dinos", (req, res) => {
+  res.status(200).json(dinosJSON)
+});
 
-function dinoController (req, res) => {
+app.post("/dinos", (req, res) => {
+  const dino = req.body;
+  dinosJSON.push(dino)
+  res.status(201).send("ok!")
+});
+
+app.get("*", (req, res) => {
+  res.status(404).json({
+    message: "Sorry, Page not found!"
+  });
+});
+
+app.listen(4000, () => {
+  console.log("Express server is now running on port 4000")
+})
+
+function dinoController(req, res) => {
 
 }
 
 
 
-export {};
+export { };
