a2ad6bc86a4be5a6f2b6d68b18c085011908cefe
Crystal Mackey <moonrunner575@gmail.com>
2021-01-05 16:21:39 -0500

    remove rest of unnecessary function

--- backend/main.js
+++ backend/main.js
@@ -2,7 +2,7 @@ import express, { urlencoded } from "express";
 import cors from "cors";
 import "./utils";
 import bodyParser from "body-parser";
-import adaptRequest from "./helpers/adapt-request"
+
 
 const app = express()
 app.use(bodyParser.json())
