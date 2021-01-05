48eb57da8d47aa4913854a5ab239a3a26bb909e8
JenMcDonnell <jennymcdonn@gmail.com>
2021-01-05 09:57:37 -0500

    Added navigation into Navigation.js -- commented out for time being

---
+++ frontend/src/components/navigation/Navigation.js
@@ -0,0 +1,20 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import "./Navigation.css";
+
+// function Navigation(props) {
+//   return (
+//     <div className="links">
+//       <div className="home">
+//         <Link to="/">Home</Link>
+//       </div>
+//       <div className="dinosaur-link">
+//         <Link to="/dinosaur">Dinosaur input</Link>
+//       </div>
+//      <div className="profile-link">
+//         <Link to="/dinosaur">Profile</Link>
+//       </div>
+//     </div>
+//   );
+// }
+// export default Navigation;
