6a8738539acc9c23989a29c16633747613917e8d
Crystal Mackey <moonrunner575@gmail.com>
2021-01-09 00:30:39 -0500

    homepage words and graphic

--- frontend/src/screens/Dashboard.js
+++ frontend/src/screens/Dashboard.js
@@ -1,3 +1,11 @@
+import logo from "../assets/A.gif";
+
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div>
+      <img src={logo} alt="Ark A" />
+      <br />
+      <h1>Welcome Home</h1>
+    </div>
+  );
 }
