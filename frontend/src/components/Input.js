f93640e0a319b92d39c4ce287b1410a5211b1512
Crystal Mackey <moonrunner575@gmail.com>
2021-01-08 20:28:37 -0500

    didn't realize wrote over require property jen put in. put it back for form input

--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -162,31 +162,37 @@ const Input = () => {
           <label>Enter stats</label>
           <br />
           <label> Health</label>
-          <input type="number" value={health} pattern onChange={(e) => setHealth(e.target.value)} />
+          <input type="number" require min="1" value={health} pattern onChange={(e) => setHealth(e.target.value)} />
           <br />
           <label>Stamina</label>
-          <input type="number" value={stamina} onChange={(e) => setStamina(e.target.value)} />
+          <input type="number" require min="1" value={stamina} onChange={(e) => setStamina(e.target.value)} />
           <br />
           <label>Oxygen</label>
-          <input type="number" value={oxygen} onChange={(e) => setOxygen(e.target.value)} />
+          <input type="number" require min="1" value={oxygen} onChange={(e) => setOxygen(e.target.value)} />
           <br />
           <label>Food</label>
-          <input type="number" value={food} onChange={(e) => setFood(e.target.value)} />
+          <input type="number" require min="1" value={food} onChange={(e) => setFood(e.target.value)} />
           <br />
           <label>Weight</label>
-          <input type="number" value={weight} onChange={(e) => setWeight(e.target.value)} />
+          <input type="number" require min="1" value={weight} onChange={(e) => setWeight(e.target.value)} />
           <br />
           <label>Melee Damage</label>
-          <input type="number" value={meleeDamage} onChange={(e) => setMeleeDamage(e.target.value)} />
+          <input type="number" require min="1" value={meleeDamage} onChange={(e) => setMeleeDamage(e.target.value)} />
           <br />
           <label>Movement Speed</label>
-          <input type="number" value={movementSpeed} onChange={(e) => setMovementSpeed(e.target.value)} />
+          <input
+            type="number"
+            require
+            min="1"
+            value={movementSpeed}
+            onChange={(e) => setMovementSpeed(e.target.value)}
+          />
           <br />
           <label>Torpidity</label>
-          <input type="number" value={torpidity} onChange={(e) => setTorpidity(e.target.value)} />
+          <input type="number" require min="1" value={torpidity} onChange={(e) => setTorpidity(e.target.value)} />
           <br />
           <label>Imprinting</label>
-          <input type="number" value={imprinting} onChange={(e) => setImpriniting(e.target.value)} />
+          <input type="number" require min="1" value={imprinting} onChange={(e) => setImpriniting(e.target.value)} />
           <br />
           <input type="submit" value="Submit" />
         </div>
