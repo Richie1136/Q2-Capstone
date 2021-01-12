4265f6b6278bd453e421dade3719402fb25c4f1f
Richie1136 <Richiemajor1@gmail.com>
2021-01-12 13:55:23 -0500

    Edits

--- frontend/src/screens/Dashboard.js
+++ frontend/src/screens/Dashboard.js
@@ -1,4 +1,5 @@
 import logo from "../assets/A.gif";
+<<<<<<< HEAD
 import Links from "../components/navigation/Links";
 
 export default function Dashboard() {
@@ -11,5 +12,15 @@ export default function Dashboard() {
         <h1>Welcome Home</h1>
       </div>
     </>
+=======
+
+export default function Dashboard() {
+  return (
+    <div>
+      <img src={logo} alt="Ark A" />
+      <br />
+      <h1>Welcome Home</h1>
+    </div>
+>>>>>>> 6a87385... homepage words and graphic
   );
 }
