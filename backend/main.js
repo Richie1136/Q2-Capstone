47e7e0ba178904c102276b64433b4b09db9d2a65
Richie1136 <Richiemajor1@gmail.com>
2021-01-12 14:00:42 -0500

    Edits

--- backend/main.js
+++ backend/main.js
@@ -12,8 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const MongoClient = require("mongodb").MongoClient;
+<<<<<<< HEAD
 const uri = "mongodb+srv://Kenzie8:Kenzie2021@ark-stat-app01.tp6cj.mongodb.net/test";
 const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+=======
+const uri = "mongodb+srv://Ark_Kapstone8:ArkApp2021@dino-data.2cygx.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+>>>>>>> efdbc78... mongoDB setup in backend
 
 app.get("/", (req, res) => {
   res.send("homepage");
@@ -30,6 +35,7 @@ app.get("/user", (req, res) => {
 //   });
 // });
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 app.get("/library", (req, res) => {
   client.connect((err) => {
@@ -59,15 +65,40 @@ app.post("/library", (req, res) => {
 app.all("./dinos");
 app.get("./dinos/:id");
 
+=======
+// res.status(200).json("This is working");
+>>>>>>> efdbc78... mongoDB setup in backend
 app.get("/library", (req, res) => {
-  res.status(200).json("This is working");
+  client.connect((err) => {
+    const collection = client.db("test").collection("dino-data");
+    collection.find().toArray((err, documents) => {
+      if (err) {
+        throw err;
+      }
+      res.send(documents);
+    });
+    client.close();
+  });
 });
 
 app.post("/library", (req, res) => {
+<<<<<<< HEAD
   const dino = req.body;
   dinosJSON.push(dino);
   res.status(201).send("ok!");
 >>>>>>> a994305... Working on API and navbar
+=======
+  client.connect((err) => {
+    const collection = client.db("test").collection("dino-data");
+    collection.insertOne(req.body, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.send(result.insertedId);
+    });
+    client.close();
+  });
+>>>>>>> efdbc78... mongoDB setup in backend
 });
 
 app.get("*", (req, res) => {
