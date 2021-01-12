49455472996e1d76d385a7a7341da05509ee3151
Richie1136 <Richiemajor1@gmail.com>
2021-01-12 14:29:28 -0500

    Fixing my project

--- backend/main.js
+++ backend/main.js
@@ -12,18 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const MongoClient = require("mongodb").MongoClient;
-<<<<<<< HEAD
-<<<<<<< HEAD
-const uri = "mongodb+srv://Kenzie8:Kenzie2021@ark-stat-app01.tp6cj.mongodb.net/test";
-const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true });
-=======
+
 const uri = "mongodb+srv://Ark_Kapstone8:ArkApp2021@dino-data.2cygx.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
->>>>>>> efdbc78... mongoDB setup in backend
-=======
-const uri = "mongodb+srv://Kenzie8:Kenzie2021@ark-stat-app01.tp6cj.mongodb.net/test";
-const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true });
->>>>>>> 3f16834... updated uri for main.js
 
 app.get("/", (req, res) => {
   res.send("homepage");
@@ -39,10 +30,6 @@ app.get("/user", (req, res) => {
 //     res.redirect("/");
 //   });
 // });
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 app.get("/library", (req, res) => {
   client.connect((err) => {
     const collection = client.db("values").collection("dinos");
@@ -67,56 +54,52 @@ app.post("/library", (req, res) => {
     });
     client.close();
   });
-=======
-app.all("./dinos");
-app.get("./dinos/:id");
 
-=======
-// res.status(200).json("This is working");
->>>>>>> efdbc78... mongoDB setup in backend
-=======
->>>>>>> 3f16834... updated uri for main.js
-app.get("/library", (req, res) => {
-  client.connect((err) => {
-    const collection = client.db("values").collection("dinos");
-    collection.find().toArray((err, documents) => {
-      if (err) {
-        throw err;
-      }
-      res.send(documents);
+  app.all("./dinos");
+  app.get("./dinos/:id");
+
+
+  // res.status(200).json("This is working");
+
+  app.get("/library", (req, res) => {
+    client.connect((err) => {
+      const collection = client.db("values").collection("dinos");
+      collection.find().toArray((err, documents) => {
+        if (err) {
+          throw err;
+        }
+        res.send(documents);
+      });
+      client.close();
     });
-    client.close();
   });
-});
 
-app.post("/library", (req, res) => {
-<<<<<<< HEAD
-  const dino = req.body;
-  dinosJSON.push(dino);
-  res.status(201).send("ok!");
->>>>>>> a994305... Working on API and navbar
-=======
-  client.connect((err) => {
-    const collection = client.db("values").collection("dinos");
-    collection.insertOne(req.body, (err, result) => {
-      if (err) {
-        throw err;
-      }
-      res.send(result.insertedId);
+  app.post("/library", (req, res) => {
+
+    const dino = req.body;
+    dinosJSON.push(dino);
+    res.status(201).send("ok!");
+
+    client.connect((err) => {
+      const collection = client.db("values").collection("dinos");
+      collection.insertOne(req.body, (err, result) => {
+        if (err) {
+          throw err;
+        }
+        res.send(result.insertedId);
+      });
+      client.close();
     });
-    client.close();
   });
->>>>>>> efdbc78... mongoDB setup in backend
-});
 
-app.get("*", (req, res) => {
-  res.status(404).json({
-    message: "Sorry, Page not found!",
+  app.get("*", (req, res) => {
+    res.status(404).json({
+      message: "Sorry, Page not found!",
+    });
   });
-});
 
-app.listen(4000, () => {
-  console.log("Express server is now running on port 4000");
-});
+  app.listen(4000, () => {
+    console.log("Express server is now running on port 4000");
+  });
 
-export {};
+  export { };
