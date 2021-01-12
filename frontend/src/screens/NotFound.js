81577569b04a8a066f30a6873a64a3b677c7d2c9
Richie1136 <Richiemajor1@gmail.com>
2021-01-12 13:53:44 -0500

    Edits

--- frontend/src/screens/NotFound.js
+++ frontend/src/screens/NotFound.js
@@ -1,4 +1,5 @@
 import logo from "../assets/wyvern.gif";
+<<<<<<< HEAD
 import Links from "../components/navigation/Links";
 
 export default function NotFound() {
@@ -11,5 +12,15 @@ export default function NotFound() {
         <h1>You Should Leave this place......</h1>
       </div>
     </>
+=======
+
+export default function NotFound() {
+  return (
+    <div>
+      <img src={logo} alt="You should leave" />
+      <br />
+      <h1>You Should Leave this place......</h1>
+    </div>
+>>>>>>> 00160cd... 404 not found page graphic and message with test link to prove it works
   );
 }
