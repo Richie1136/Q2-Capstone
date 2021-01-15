dcd5e3ff4f1f9bcb21314a9871928bf8b7bc0c04
JenMcDonnell <jennymcdonn@gmail.com>
2021-01-15 16:35:54 -0500

    Created redux store

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,5 +1,7 @@
 import useMagicLink from "use-magic-link";
+//import action creator
 
 export default function useAuth() {
+  //call action creator
   return useMagicLink("pk_test_C8D41DCE9999FDC6");
 }
